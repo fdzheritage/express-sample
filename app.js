e8310b3cfdb3aa512599d994f858a1629a099368
Fernandez del Castil <FernandezDelCastillo.Francisco@cegep-heritage.qc.ca>
2019-04-08 12:00:19 -0400

    Updated to the last version from Lucas

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 const express = require("express");
 const app = express();
-
 const port = 9000;
 
-
-app.get('/', (req, res)=> {
-    res.send('<h1>Hello World</h1>');
+app.get("/bye", (req, res) => {
+	return res.send("bubye!");
 });
 
-app.get('/home', (req,res)=> {
-    res.send('<h1>Your are home</h1>');
+app.get("/hello", (req, res) => res.send("hey there"));
+
+app.get("/users/:userId/:email", (req, res) => {
+	let uid = parseInt(req.params.userId);
+	let email = req.params.email;
+
+	res.send(`user ${uid}'s email set to ${email}`);
 });
 
-app.get('/user/:username', (req,res)=> {
-    res.send('Welcome: ' + req.params.username);
+app.get("/numbers/:from-:to", (req, res) => {
+	let a = parseInt(req.params.from);
+	let b = parseInt(req.params.to);
+
+	let html = "";
+
+	if (a > b) {
+		let t = a;
+		a = b;
+		b = t;
+	}
+
+	for (let x = a; x <= b; x++) {
+		html += `<div>${x}</div>`;
+	}
+
+	res.send(html);
 });
 
-app.listen(port, 
-    ()=>{
-        console.log(`Sample App ${port}!`);
-    }
-);
+app.listen(port, function() {
+	return console.log("Sample App " + port + "!");
+});
