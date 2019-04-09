8d729c8f0ea456b579ad2326506e9a5b261350b7
Fernandez del Castil <FernandezDelCastillo.Francisco@cegep-heritage.qc.ca>
2019-04-09 09:55:07 -0400

    Added users router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const app = express();
+const usersRouter = require('./routes/users.js');
 const port = 9000;
 
 app.use(cookieParser());
@@ -17,6 +18,8 @@ app.use((req, res, next) => {
 	next();
 });
 
+app.use('/users', usersRouter);
+
 app.get("/inc", (req, res) => {
 	return res.send(path.join(__dirname, "inc"));
 });
@@ -24,32 +27,6 @@ app.use("/inc", express.static(path.join(__dirname, "inc")));
 
 app.get("/hello", (req, res) => {
 	return res.send(req.vcount + "<br>hey there");
-});
-
-app.get("/users/:userId/:email", (req, res) => {
-	let uid = parseInt(req.params.userId);
-	let email = req.params.email;
-
-	res.send(`user ${uid}'s email set to ${email}`);
-});
-
-app.get("/numbers/:from-:to", (req, res) => {
-	let a = parseInt(req.params.from);
-	let b = parseInt(req.params.to);
-
-	let html = "";
-
-	if (a > b) {
-		let t = a;
-		a = b;
-		b = t;
-	}
-
-	for (let x = a; x <= b; x++) {
-		html += `<div>${x}</div>`;
-	}
-
-	res.send(html);
 });
 
 app.listen(port, function() {
---
+++ routes/numbers.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+router.get("/numbers/:from-:to", (req, res) => {
+	let a = parseInt(req.params.from);
+	let b = parseInt(req.params.to);
+
+	let html = "";
+
+	if (a > b) {
+		let t = a;
+		a = b;
+		b = t;
+	}
+
+	for (let x = a; x <= b; x++) {
+		html += `<div>${x}</div>`;
+	}
+
+	res.send(html);
+});
+
+module.exports = router;
