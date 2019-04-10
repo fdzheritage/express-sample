c837f92802674bddec27d727c7629dfa54e24b48
Fernandez del Castil <FernandezDelCastillo.Francisco@cegep-heritage.qc.ca>
2019-04-10 11:05:45 -0400

    Forgot something

--- model/navmenu.js
+++ model/navmenu.js
@@ -3,7 +3,6 @@ const db = require('../config/db');
 module.exports = class {
     static async getNavMenu(pageKey) {
         let connection = await db.getConnection();
-        // TODO: check query
         const rows = await connection.query("SELECT pageKey, label FROM `navmenu`, page WHERE page.pageId=navmenu.pageId ORDER BY menuOrder");
         connection.end();
         return rows;
